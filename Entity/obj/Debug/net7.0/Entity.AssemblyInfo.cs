c1f57f5d30b92ceb4a76863e45f4ec0d5baa5214
Emirhan DOĞRU <emirhan-dogru@hotmail.com>
2024-10-10 10:08:23 +0300

    entities created

--- Entity/obj/Debug/net7.0/Entity.AssemblyInfo.cs
+++ Entity/obj/Debug/net7.0/Entity.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32b3b476a9b0536cebc41faeaf6e98ceba02b198")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
